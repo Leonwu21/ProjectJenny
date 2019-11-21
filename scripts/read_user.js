eba7db3523716ef55d47ffd418bf0c519aa03367
Scott Lai <tuonglai3602@gmail.com>
2019-11-20 21:51:37 -0800

    add create_group.js and comment read_user.js

--- scripts/read_user.js
+++ scripts/read_user.js
@@ -3,6 +3,10 @@
 function showName() {
 
   firebase.auth().onAuthStateChanged(function (user) {
+    // if the current user logged in 
+    // user is authenticated, then grab the "uid" to access the
+    // current user's document in fire base to display the user name
+    // in index.html
     db.collection("users/").doc(user.uid).onSnapshot(function (d) {
         if (d.get('name') != null) {
           document.getElementById('name').innerHTML = d.data()["name"];
