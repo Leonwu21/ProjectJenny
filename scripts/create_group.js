8f5b3db941847f5cb0134f54899e2b9a10d18b3a
Scott Lai <tuonglai3602@gmail.com>
2019-11-22 13:45:54 -0800

    edit creategroup.html and creategroup.js

--- scripts/create_group.js
+++ scripts/create_group.js
@@ -1,3 +1,9 @@
 function createGroup() {
-  // TODO
+  // user's inputs from the web page
+  var groupName = document.getElementById("name").value;
+  var groupCourse = document.getElementById("course").value;
+  var groupDay = document.getElementById("day").value;
+  var groupSlot = document.getElementById("slot").value;
+
+  
 }
