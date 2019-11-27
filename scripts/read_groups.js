3f8119772947b6cdacc9a5afa57ea20f3a9b7b7c
Kurt Milan <kdmilan724@gmail.com>
2019-11-26 16:54:47 -0800

    find_groups.js now working

--- scripts/read_groups.js
+++ scripts/read_groups.js
@@ -16,24 +16,24 @@ function getGroups() {
 }
 
 function findGroups(day, slot) {
-  db.collection("groups")
+  db.collection("groups/")
     .where("day", "==", day).where("slot", "==", slot)
     .get().then(function (snap) {
-      let i = 0;
-      let j = 100;
-      let h = 10000;
+      
       snap.forEach(function (doc) {
         let groupName = doc.data().name;
-        let groupDay = doc.data().day;
-        let groupSlot = doc.data().slot;
+        let groupDay = doc.data()['day'];
+        let groupSlot = doc.data()['slot'];
         let groupCourse = doc.data().course;
-        let newDiv = $("<div class='group'><span id='" + i + "' class='groupName'></span>\
-          <span id='"+ j + "' class='groupTime'></span><span id='" + h + "' class='groupCourse'></span></div>");
+        // let newDiv = $("<div class='group'><span id='" + i + "' class='groupName'></span>\
+        //   <span id='"+ j + "' class='groupTime'></span><span id='" + h + "' class='groupCourse'></span></div>");
+        let newDiv = $("<div class='group'><span id='' class='groupName'>" + groupName + "</span>\
+          <span id='' class='groupTime'>"+groupDay+ " " + groupSlot+ "</span><span id='' class='groupCourse'>"+ groupCourse + "</span></div>");
         $("#content").append(newDiv);
-        $("'#" + i + "'").append(groupName);
-        $("'#" + j + "'").append(groupDay);
-        $("'#" + j + "'").append(groupSlot);
-        $("'#" + h + "'").append(groupCourse);
+        // $("'#" + i + "'").append(groupName);
+        // $("'#" + j + "'").append(groupDay);
+        // $("'#" + j + "'").append(groupSlot);
+        // $("'#" + h + "'").append(groupCourse);
         i++;
         j++;
         h++;
@@ -43,7 +43,7 @@ function findGroups(day, slot) {
 
 function showGroups() {
   firebase.auth().onAuthStateChanged(function (user) {
-    db.collection("users").doc(user.uid).collection("freeslot").get()
+    db.collection("users/").doc(user.uid).collection("freeslot").get()
       .then(function (snap) {
         snap.forEach(function (doc) {
           var dayfree = doc.data().day;
