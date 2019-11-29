f69e094490d5ff5498ec7bd4f9b40fb47540e12b
kurtbot <56173650+kurtbot@users.noreply.github.com>
2019-11-29 03:28:38 -0800

    updated edit_profile.js and edit_profile.html to show current day

--- scripts/edit_profile.js
+++ scripts/edit_profile.js
@@ -1,10 +1,51 @@
+$(document).ready(function () {
 
-function progressBar(){
-    $('#nextProfDat').click(function(){
-        
+    $('#day').text("Monday");
+    $('#currentDay').text("Monday");
+
+    progressBar()
+});
+
+
+// Global Variables
+var progress = 1;
+
+function progressBar() {
+    $('#nextProfDat').click(function () {
+        console.log("Clicked");
+        ++progress;
+        $('#prof-progress div').css("width", 20 * progress + "%");
+
+        switch (progress) {
+            case 1:
+                $('#day').text("Monday");
+                $('#currentDay').text("Monday");
+                break;
+            case 2:
+                $('#day').text("Tuesday");
+                $('#currentDay').text("Tuesday");
+                break;
+            case 3:
+                $('#day').text("Wednesday");
+                $('#currentDay').text("Wednesday");
+                break;
+            case 4:
+                $('#day').text("Thursday");
+                $('#currentDay').text("Thursday");
+                break;
+            case 5:
+                $('#day').text("Friday");
+                $('#currentDay').text("Friday");
+                break;
+            case 6:
+                location.href = "./index.html";
+                break;
+        }
+
     });
 }
-function setTime() {
+
+function setTime(dayval, slotval) {
 
     var userDay = document.getElementById("day").value;
     var userSlot = document.getElementById("time").value;
@@ -13,3 +54,4 @@ function setTime() {
         var db = db.collection("users").doc(user.uid).collection("freeslot");
     });
 }
+
